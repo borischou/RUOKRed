4f6604b6826b82e6315c5f414bb1c0c7086cd41f
周博立 <bolizhou@xiaohongshu.com>
2019-01-28 16:12:03 +0800

    pure

---
+++ RUOKRed/RUOKRedObject.m
@@ -0,0 +1,18 @@
+//
+//  RUOKRedObject.m
+//  RUOKRed
+//
+//  Created by 周博立 on 2019/1/28.
+//  Copyright © 2019 周博立. All rights reserved.
+//
+
+#import "RUOKRedObject.h"
+
+@implementation RUOKRedObject
+
+- (NSString *)hello
+{
+    return @"hello world";
+}
+
+@end
